947d0fd6a802d8ca24a0d18c511b3348c9c205dd
Solomonwisdom <whg19962015@163.com>
2019-07-26 13:21:59 +0800

    NJUCWSP

---
+++ NJUCWSPTest/improve/run.bat
@@ -0,0 +1,7 @@
+python evaluate.py pku_my.txt pku_gold.utf8
+python evaluate.py msr_my.txt msr_gold.utf8
+python evaluate.py 2007_my.txt 2007_gold.utf8
+python evaluate.py final_my.txt final_gold.utf8
+python evaluate.py 2012_my.txt 2012_gold.utf8
+python evaluate.py jieba_2012_test.utf8 2012_gold.utf8
+PAUSE
---
+++ NJUCWSPTest/src/Main.java
@@ -0,0 +1,9 @@
+import Joint.Joint;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Joint joint = new Joint();
+        joint.seg_and_tag();
+    }
+}
